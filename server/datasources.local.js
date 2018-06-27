34b25ac51c5c6717d3f0084844d1ecbce9b7d2be
Shane <shane.mcnair@gmail.com>
2018-06-27 11:44:20 -0700

    first commit

---
+++ server/datasources.local.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "db": {
+    "name": "db",
+    "connector": "memory"
+  },
+
+  "mongoDB": {
+    "host": "",
+    "port": 0,
+    "url": "",
+    "database": "",
+    "password": "",
+    "name": "mongoDB",
+    "user": "",
+    "connector": "mongodb"
+  }
+}
